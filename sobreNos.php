77d116511a8c1f9606a8601fc4eb7c582a7e7181
Straav12 <71297724+Straav12@users.noreply.github.com>
2022-12-02 14:27:22 -0300

    Add files via upload

---
+++ sobreNos.php
@@ -0,0 +1,120 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="./css/sobreNos.css">
+    <link rel="stylesheet" type="text/css" href="./css/slick.css">
+    <title>Lixeia | Sobre nós</title>
+</head>
+
+<body>
+    <header class="menu-principal">
+        <main>
+            <div class="header-1">
+                <div class="logo">
+                    <a href="index.php">
+                        <img src="./img/Logo Lixeia.png" />
+                    </a>
+                </div>
+                <div class="redes-sociais">
+                    <ul>
+                        <li>
+                            <a href="https://pt-br.facebook.com/" target="blank">
+                                <!--as referencias sao os links das redes sociais da lixeia-->
+                                <img src="./img/Logo Facebook.png">
+                                <!--as sources sao os logos das redes sociais correspondentes-->
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/" target="blank">
+                                <!--as referencias sao os links das redes sociais da lixeia-->
+                                <img src="./img/Logo Instagram.png">
+                                <!--as sources sao os logos das redes sociais correspondentes-->
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://br.linkedin.com/" target="blank">
+                                <!--as referencias sao os links das redes sociais da lixeia-->
+                                <img src="./img/Logo Linkedin.png">
+                                <!--as sources sao os logos das redes sociais correspondentes-->
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/" target="blank">
+                                <!--as referencias sao os links das redes sociais da lixeia-->
+                                <img src="./img/Logo Twitter.png">
+                                <!--as sources sao os logos das redes sociais correspondentes-->
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    </header>
+    <main class="col-100 menu-urls">
+        <div class="header-2">
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <!--as referencias sao os links das paginas que cada item do menu direcionara-->
+                    <li>
+                        <a href="">Nosso Serviço</a>
+                    </li>
+                    <!--as referencias sao os links das paginas que cada item do menu direcionara-->
+                    <li>
+                        <a href="">Aluguel</a>
+                    </li>
+                    <!--as referencias sao os links das paginas que cada item do menu direcionara-->
+                    <li>
+                        <a href="sobreNos.php">Sobre Nós</a>
+                    </li>
+                    <!--as referencias sao os links das paginas que cada item do menu direcionara-->
+                    <li>
+                        <a href="">Contato</a>
+                    </li>
+                    <!--as referencias sao os links das paginas que cada item do menu direcionara-->
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                    <!--as referencias sao os links das paginas que cada item do menu direcionara-->
+                </ul>
+            </div>
+            <div class="busca">
+                <input placeholder="Pesquisar no Site" type="text">
+            </div>
+        </div>
+    </main>
+    <div class="col-100">
+        <div class="content texto-destaque">
+            <h1> <strong>Quem</strong> somos nós?</h1>
+            <p> Texo sobre como nos conhecemos e apresentação geral (jovens, rio grande do sul, etc)
+            </p>
+            <div class="col-3 bloco-texto">
+                <i><img src="./img/fotosNossas/daniel.jpg" width="300" /></i>
+                
+                <h3>Daniel Kroeff</h3>
+                <p>Texto de apresentação
+                </p>
+            </div>
+            <div class="col-3 bloco-texto">
+                <i><img src="./img/fotosNossas/eduardo.jpg" width="300" /></i>
+                <h3>Eduardo Trindade</h3>
+                <p>Texto de apresentação
+                </p>
+            </div>
+            <div class="col-3 bloco-texto">
+            <i><img src="./img/fotosNossas/henrique.jpg" width="300" height="300" /></i>
+                <h3>Henrique Sangiovanni </h3>
+                <p>Texto de apresentação
+                </p>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" src="./js/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="./js/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="./js/slick.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+
+</html>
