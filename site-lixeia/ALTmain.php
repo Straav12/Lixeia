464fe83b62dfb75bfe1fde9d410c3642dd786542
Straav12 <71297724+Straav12@users.noreply.github.com>
2022-12-05 23:26:45 -0300

    Add files via upload

--- site-lixeia/ALTmain.php
+++ site-lixeia/ALTmain.php
@@ -1,6 +1,3 @@
-<?php
-include ('ALTmainscript.php');
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -62,7 +59,6 @@ include ('ALTmainscript.php');
                 <input type="text" name="email" placeholder="E-mail">
             </p>
             <?php 
-              //  include ('ALTmainscript.php');
                 include ('ALTconexao.php');
 
                 if(isset($_POST['email']) || isset($_POST['senha'])){
@@ -76,7 +72,6 @@ include ('ALTmainscript.php');
                 <input type="password" name="senha" placeholder="Senha">
             </p>
             <?php
-               // include ('ALTmainscript.php');
                 include ('ALTconexao.php');
 
                 if(isset($_POST['email']) || isset($_POST['senha'])){
@@ -88,6 +83,48 @@ include ('ALTmainscript.php');
             <p>
                 <button class="inputSubmit" type="submit">Entrar</button>
             </p>
+            <?php
+            //método para o login
+                if(isset($_POST['email']) || isset($_POST['senha'])){
+
+                    if(strlen($_POST['email']) == 0){  
+                        $tememail = 0;
+                    }else if(strlen($_POST['senha']) == 0){
+                        $temsenha = 0;
+                    }else{
+                
+                        $tememail = 1;
+                        $temsenha = 1;
+                
+                        $email = $mysqli->real_escape_string($_POST['email']);
+                        $senha = $mysqli->real_escape_string($_POST['senha']);
+                        
+                        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+                        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+                
+                        $quantidade = $sql_query->num_rows;
+                
+                        if($quantidade == 1){
+                            
+                            $usuario = $sql_query->fetch_assoc();
+                
+                            if(!isset($_SESSION)){
+                                session_start();
+                            }
+                
+                            $_SESSION['id'] = $usuario['id'];
+                            $_SESSION['nome'] = $usuario['nome'];
+                
+                            header("Location: ALTpainel.php");
+                
+                        }else{
+                            //dados invalidos
+                            echo "<p style='color:red;'>Dados inválidos.</p>";
+                        }
+                
+                    }
+                }
+            ?>
             <br>
             <a href="index.php" class="back">Voltar</a>
             
