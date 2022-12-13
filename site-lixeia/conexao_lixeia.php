245772397be9b4ae29a9a91da87d9a2e10d75ea7
Straav12 <71297724+Straav12@users.noreply.github.com>
2022-12-13 14:46:44 -0300

    Delete site-lixeia directory

--- site-lixeia/conexao_lixeia.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbHost = 'Localhost';
-$dbUsername = 'root';
-$dbPassword = '';
-$dbName = 'db_lixeia';
-
-$conexao_lixeia = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
--- site-lixeia/conexao_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conexao = mysqli_connect('localhost:3306', 'root', '', 'loja');
-$dbHost = 'Localhost';
-$dbUsername = 'root';
-$dbPassword = '';
-$dbName = 'formulario-login';
-
-$conexao_login = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
